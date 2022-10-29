45b6065f4ef72d3e1afde0f508ffe1a3dffb86bc
khames94 <khamis.abdelsatar1@gmail.com>
2022-10-30 00:59:29 +0300

    add ponus apis

--- src/main/java/com/lendo/controller/FetchingDataController.java
+++ src/main/java/com/lendo/controller/FetchingDataController.java
@@ -1,6 +1,7 @@
 package com.lendo.controller;
 
 import java.net.URISyntaxException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,10 +9,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.lendo.model.User;
+import com.lendo.model.Comment;
+import com.lendo.model.Post;
 import com.lendo.service.DataDetailsService;
 
 @RestController
@@ -39,6 +39,17 @@ public class FetchingDataController {
 		return new ResponseEntity<>(service.getComments(), HttpStatus.OK);
 	}
 	
+	 @GetMapping("/users/{id}")
+	 List<Post> getPostsForUser( @PathVariable("id") long id) throws Exception {
+	    
+	    return service.getPostsForUser(id);
+	  }
+
+	 @GetMapping("/comment/{id}")
+	 List<Comment> getPostComments( @PathVariable("id") long id) throws Exception {
+	    
+	    return service.findCommentsByPostId(id);
+	  }
 
 
 }
--- src/main/java/com/lendo/service/DataDetailsService.java
+++ src/main/java/com/lendo/service/DataDetailsService.java
@@ -2,8 +2,13 @@ package com.lendo.service;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.apache.catalina.mapper.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.annotation.JsonFormat.Feature;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lendo.model.Comment;
@@ -32,43 +38,62 @@ public class DataDetailsService {
 	@Autowired
 	private ObjectMapper mapper;
 
+	//Get all the users
 	@SneakyThrows
-	public List<User>  getUsers() throws URISyntaxException {
+	public List<User> getUsers() throws URISyntaxException {
 		URI uri = new URI(baseurl + "/users");
 		ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
-		List<User> usersList = mapper.readValue(result.getBody(), new TypeReference<List<User>>(){});
+		List<User> usersList = mapper.readValue(result.getBody(), new TypeReference<List<User>>() {
+		});
 		log.info("==users:" + result);
 		return usersList;
 	}
 
+	//Get all the posts
 	@SneakyThrows
 	public List<Post> getPosts() throws URISyntaxException {
 		URI uri = new URI(baseurl + "/posts");
 		ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
-		List<Post> PostList = mapper.readValue(result.getBody(), new TypeReference<List<Post>>(){});
+		List<Post> PostList = mapper.readValue(result.getBody(), new TypeReference<List<Post>>() {
+		});
 		log.info("==Posts:" + result);
 		return PostList;
 	}
-	
+
+	//Get all the comments
 	@SneakyThrows
 	public List<Comment> getComments() throws URISyntaxException {
 		URI uri = new URI(baseurl + "/comments");
 		ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
-		List<Comment> PostList = mapper.readValue(result.getBody(), new TypeReference<List<Comment>>(){});
+		List<Comment> PostList = mapper.readValue(result.getBody(), new TypeReference<List<Comment>>() {
+		});
 		log.info("==Comments:" + result);
 		return PostList;
 	}
 
-	public User findUserById(Long id) throws Exception {
-		
-		List<User>  users=getUsers();
-		
-		Optional<User> matchingObject = users.stream().
-			    filter(user -> user.getId().equals(id)).
-			    findFirst();
-		return matchingObject.get();
-		
-		
+	//Filter the posts for a user
+	public List<Post> getPostsForUser(long userId) throws Exception {
+
+		List<Post> response = new ArrayList<>();
+		List<Post> postsList = getPosts();
+		for (Post post : postsList) {
+			if (post.getUser_id().equalsIgnoreCase(String.valueOf(userId)))
+				response.add(post);
+		}
+
+		return response;
+
+	}
+ //Filter the comments of a post
+	public List<Comment> findCommentsByPostId(long id) throws URISyntaxException {
+		List<Comment> response = new ArrayList<>();
+		List<Comment> commentsList = getComments();
+		for (Comment comment : commentsList) {
+			if (comment.getPost_id().equalsIgnoreCase(String.valueOf(id)))
+				response.add(comment);
+		}
+
+		return response;
 	}
 
 }
